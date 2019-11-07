dd3c93d9eacb190223898c40067618dcbfe3cf0e
1GeanChow1 <senxinzou@163.com>
2019-11-07 18:45:08 +0800

    soap

---
+++ src/main/java/com/example/mail/MailRequest.java
@@ -0,0 +1,120 @@
+
+package com.example.mail;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>anonymous complex type鐨� Java 绫汇��
+ * 
+ * <p>浠ヤ笅妯″紡鐗囨鎸囧畾鍖呭惈鍦ㄦ绫讳腑鐨勯鏈熷唴瀹广��
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="address" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="subject" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="information" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "address",
+    "subject",
+    "information"
+})
+@XmlRootElement(name = "MailRequest", namespace = "http://www.howtodoinjava.com/xml/school")
+public class MailRequest {
+
+    @XmlElement(namespace = "http://www.howtodoinjava.com/xml/school", required = true)
+    protected String address;
+    @XmlElement(namespace = "http://www.howtodoinjava.com/xml/school", required = true)
+    protected String subject;
+    @XmlElement(namespace = "http://www.howtodoinjava.com/xml/school", required = true)
+    protected String information;
+
+    /**
+     * 
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * 
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAddress(String value) {
+        this.address = value;
+    }
+
+    /**
+     * 
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * 
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSubject(String value) {
+        this.subject = value;
+    }
+
+    /**
+     * 
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getInformation() {
+        return information;
+    }
+
+    /**
+     *
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setInformation(String value) {
+        this.information = value;
+    }
+
+}
